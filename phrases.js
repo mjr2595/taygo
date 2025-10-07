5e83ae77646e8a878b6800f330f28f4d6d7f2ede
mjr2595 <michaeljohnraymond@gmail.com>
2025-10-07 08:09:47 -0500

    formatting

--- phrases.js
+++ phrases.js
@@ -1,56 +1,57 @@
 // Taygo phrases - can appear in any column
 const phrases = [
-    "appwrite hero",
-    "mentions not being technical",
-    "pepsi pat",
-    "mentions hiring jason",
-    "\"chat is blowing up / crazy\"",
-    "\"awesome\"",
-    "service industry being this gen's war",
-    "AI mentioned",
-    "torc discord mentioned",
-    "\"i love it\"",
-    "mentions the stream being a virtual meetup",
-    "\"I would put my network up against anybody's\"",
-    "ambassador program",
-    "text updates not being spam",
-    "\"we need a part two\"",
-    "cold brew",
-    "mentions a conference talk",
-    "dads in tech mentioned",
-    "react mentioned",
-    "recruiters taking 7s to read resume",
-    ".NET mentioned",
-    "laravel mentioned",
-    "vanilla js mentioned",
-    "\"content creation\"",
-    "your comment or you get shouted out",
-    "jason roasts taylor in chat",
-    "roxy's height mentioned",
-    "brand building",
-    "taco bell mentioned",
-    "outback steakhouse mentioned",
-    "talks about sailing",
-    "mentions his wife"
+  "appwrite hero",
+  "mentions not being technical",
+  "pepsi pat",
+  "mentions hiring jason",
+  '"chat is blowing up / crazy"',
+  '"awesome"',
+  "service industry being this gen's war",
+  "AI mentioned",
+  "torc discord mentioned",
+  '"i love it"',
+  "mentions the stream being a virtual meetup",
+  '"I would put my network up against anybody\'s"',
+  "ambassador program",
+  "text updates not being spam",
+  '"we need a part two"',
+  "cold brew",
+  "mentions a conference talk",
+  "dads in tech mentioned",
+  "react mentioned",
+  "recruiters taking 7s to read resume",
+  ".NET mentioned",
+  "laravel mentioned",
+  "vanilla js mentioned",
+  '"content creation"',
+  "your comment or you get shouted out",
+  "jason roasts taylor in chat",
+  "roxy's height mentioned",
+  "brand building",
+  "taco bell mentioned",
+  "outback steakhouse mentioned",
+  "talks about sailing",
+  "mentions his wife",
 ];
 
 // Function to get random phrases for a column
 function generatePhraseColumn(usedPhrases = new Set()) {
-    const columnPhrases = [];
-    const availablePhrases = phrases.filter(phrase => !usedPhrases.has(phrase));
-    
-    // If we don't have enough unused phrases, reset and use any
-    const phrasesToUse = availablePhrases.length >= 5 ? availablePhrases : phrases;
-    
-    while (columnPhrases.length < 5) {
-        const randomIndex = Math.floor(Math.random() * phrasesToUse.length);
-        const phrase = phrasesToUse[randomIndex];
-        
-        if (!columnPhrases.includes(phrase)) {
-            columnPhrases.push(phrase);
-            usedPhrases.add(phrase);
-        }
+  const columnPhrases = [];
+  const availablePhrases = phrases.filter((phrase) => !usedPhrases.has(phrase));
+
+  // If we don't have enough unused phrases, reset and use any
+  const phrasesToUse =
+    availablePhrases.length >= 5 ? availablePhrases : phrases;
+
+  while (columnPhrases.length < 5) {
+    const randomIndex = Math.floor(Math.random() * phrasesToUse.length);
+    const phrase = phrasesToUse[randomIndex];
+
+    if (!columnPhrases.includes(phrase)) {
+      columnPhrases.push(phrase);
+      usedPhrases.add(phrase);
     }
-    
-    return columnPhrases;
+  }
+
+  return columnPhrases;
 }
